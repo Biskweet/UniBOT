89147d2da55c86b6f99d66b71b6f1f35a0cab47e
gasparddm <gaspard.dama@gmail.com>
2021-09-23 00:18:29 +0200

    apparently there is stuff to commit?

--- commands/moderation.js
+++ commands/moderation.js
@@ -45,49 +45,41 @@ export function kick(message, reason) {
 
         catch (error) {
             console.log("-------------------------\n", error, "\n-------------------------");
-            message.channel.send("Il y a eu une erreur lors de l'expulsion des membres !\n" + error.message);
+            message.channel.send("Il y a eu une erreur lors de l'expulsion du membre !\n" + error.message);
             message.react('❌');
         }
     }
 }
 
 
-export function ban(message, reason) {
+export function kick(message, reason) {
     if (utils.isModo(message.member)) {
 
+        let member = message.mentions.members.first();
+
         try {
-
-            let member = message.mentions.members.first()
-
-            if (!member.bannable) {  // Client does not have permission
-                message.react('❌');
-                message.channel.send("Permissions insuffisantes.");
-                return;
+            if (!member.kickable) {  // Client does not have permission
+                    throw new Error("Permissions insuffisantes.");
             }
-
+        
             let alert = "Vous avez été banni du serveur Discord Étudiant Sorbonne Université.";
             if (reason.length) {
                 alert += "\n\nMotif : " + reason;
             }
 
+            member.ban();
+            message.react('✅');
+
             member.send(alert)
-                .then(() => {
-                    member.ban();
-                })
-                .catch(() => {
-                    console.log("Error when sending message.") 
-                    member.ban()
-                        .then(console.log)
-                });
+                .catch((err) => {console.log(`Could not send kick alert to ${member.user.tag}`)})
+
         }
 
         catch (error) {
             console.log("-------------------------\n", error, "\n-------------------------");
-            message.author.send("Il y a eu une erreur lors du bannissement du membre !");
+            message.channel.send("Il y a eu une erreur lors du bannissement du membre !\n" + error.message);
             message.react('❌');
-            return;
-        };
-        message.react('✅');
+        }
     }
 }
 
