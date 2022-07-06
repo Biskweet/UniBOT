9595d60ffa3dae9c12914bb5dd55a6bb8336c289
gasparddm <gaspard.dama@gmail.com>
2022-07-06 09:48:06 +0200

    Better error handling for incorrect argument inputs

--- moderation/sendDM.js
+++ moderation/sendDM.js
@@ -3,6 +3,14 @@ const utils = require("../utils/utils.js");
 
 module.exports = async (message, args) => {
     args = args.slice(2);
+
+    if (args.length < 2) {
+        message.reply("Nombre d'arguments incorrect (merci de préciser l'ID du destinataire __puis__ le message à envoyer.");
+        message.react("❌");
+        return;
+    }
+
+
     let target = args[0];
     let messageContent = args.slice(1).join(' ');
 
